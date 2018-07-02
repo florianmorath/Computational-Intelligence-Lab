f650d5f2e53ec055536a6b7f8f35a9788eb9597c
Florian Morath <morathflorian@gmail.com>
2018-07-02 08:55:57 +0200

    refactoring Deep_collaborative_filtering
    - included plotting of train + test error based on history

---
+++ src_submission/Deep_collaborative_filtering/main.py
@@ -0,0 +1,70 @@
+from model import *
+from sklearn.metrics import mean_absolute_error
+import pickle
+
+
+def rmse(y_true, y_pred):
+	return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+def parse_line(line):
+    key, value = line.split(",")
+    rating = int(value)
+    row_string, col_string = key.split("_")
+    row = int(row_string[1:])
+    col = int(col_string[1:])
+    return row, col, rating
+
+
+
+# get the data
+train, test, max_user, max_work = get_data()
+
+# get the model
+model = get_model(max_work, max_user)
+
+# train the model
+history = model.fit([get_array(train["movieId"]), get_array(train["userId"])], get_array(train["rating"]), nb_epoch=20,
+                    validation_split=0.2, verbose=2)
+
+# TODO: create plot based on history -> https://chrisalbon.com/deep_learning/keras/visualize_loss_history/
+
+# Get training and test loss histories
+training_loss = history.history['loss']
+test_loss = history.history['val_loss']
+
+# Create count of the number of epochs
+epoch_count = range(1, len(training_loss) + 1)
+
+# Visualize loss history
+plt.plot(epoch_count, training_loss, 'r--')
+plt.plot(epoch_count, test_loss, 'b-')
+plt.legend(['Training Loss', 'Test Loss'])
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.savefig('keras_plot.png')
+plt.show();
+
+
+# make predictions
+predictions = model.predict([get_array(test["movieId"]), get_array(test["userId"])])
+
+# create csv submission file of predictions
+ratings_a = []
+for pred in predictions:
+    ratings_a.append(pred[0])
+
+ratings = []
+with open('sampleSubmission.csv') as file:
+    file.readline()
+    for line in file:
+        row, col, rat = parse_line(line)
+        ratings.append((row,col,rat))
+
+# Build output string
+output = "Id,Prediction\n"
+for (row, col, rat) in ratings:
+    output += "r%d_c%d,%f\n" % (row , col, ratings_a.pop(0))
+
+# Write file 
+with open('sub_5.csv', 'w') as output_file:
+    output_file.write(output)
---
+++ src_submission/Deep_collaborative_filtering/model.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from keras import backend
+from keras.models import Sequential, Model
+from keras.layers import Embedding, Reshape, Activation, Input, Dense, Flatten, Dropout
+from keras.layers.merge import Dot, multiply, concatenate
+from keras.utils import np_utils
+from keras.utils.data_utils import get_file
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import skipgrams
+from collections import defaultdict
+from sklearn.cross_validation import train_test_split
+
+ 
+def rmse(y_true, y_pred):
+	return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+
+def get_data():
+    
+    # train set
+    train = pd.read_csv("foo_train.csv")
+
+    # test set
+    test = pd.read_csv("foo_test.csv")
+
+    max_user = max(train["userId"].tolist() )
+    max_work = max(train["movieId"].tolist() )
+
+
+    return train, test, max_user, max_work
+
+
+def get_model(max_work, max_user):
+    dim_embedddings = 35
+    bias = 1
+    
+    # inputs
+    w_inputs = Input(shape=(1,), dtype='int32')
+    w = Embedding(max_work+1, dim_embedddings, name="work")(w_inputs)
+    w_bis = Embedding(max_work + 1, bias, name="workbias")(w_inputs)
+
+    # context
+    u_inputs = Input(shape=(1,), dtype='int32')
+    u = Embedding(max_user+1, dim_embedddings, name="user")(u_inputs)
+    u_bis = Embedding(max_user + 1, bias, name="userbias")(u_inputs)
+    o = multiply([w, u])
+    o = Dropout(0.5)(o)
+    o = concatenate([o, u_bis, w_bis])
+    o = Flatten()(o)
+    o = Dense(10, activation="relu")(o)
+    o = Dense(1)(o)
+
+    rec_model = Model(inputs=[w_inputs, u_inputs], outputs=o)
+    rec_model.compile(loss='mae', optimizer='adam', metrics=[rmse])
+
+    return rec_model
+
+def get_array(series):
+    return np.array([[element] for element in series])
