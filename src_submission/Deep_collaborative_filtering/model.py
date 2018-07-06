71b41033797cecd6bb2bac04860552763f479a60
lucienschaller <slucien@student.ethz.ch>
2018-07-06 17:04:20 +0200

    added some doc to model.py in deep rs

--- src_submission/Deep_collaborative_filtering/model.py
+++ src_submission/Deep_collaborative_filtering/model.py
@@ -1,3 +1,7 @@
+"""
+This module contains some functions some helper functions for our deep recommender system implementation.
+"""
+
 import pandas as pd
 import numpy as np
 from keras import backend
@@ -13,29 +17,51 @@ from sklearn.cross_validation import train_test_split
 
  
 def rmse(y_true, y_pred):
+    """
+    This function calculates the RMSE between the true values and the predicted values.
+    Args:
+        y_true (tensor): the true ratings
+        y_pred (tensor): the predicted ratings
+    Returns:
+        (tensor): the RMSE between y_true and y_pred
+    """
 	return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
 
 
 def get_data():
-    
+    """
+    This function reads in the training and the test data from the csv files
+    Returns:
+        (tuple): a tuple containing the training data set, the test data set as well as the maximum user and movie ids
+    """
     # train set
     train = pd.read_csv("train.csv")
 
     # test set
     test = pd.read_csv("test.csv")
 
-    max_user = max(train["userId"].tolist() )
-    max_work = max(train["movieId"].tolist() )
+    max_user_id = max(train["userId"].tolist() )
+    max_movie_id = max(train["movieId"].tolist() )
 
 
-    return train, test, max_user, max_work
+    return train, test, max_user_id, max_movie_id
 
 
-def get_model(max_work, max_user):
-    dim_embedddings = 35
+def build_model(max_movie_id, max_user_id):
+    """
+    This function builds up the model (neural network) which learn low dimensional embeddings of movies and users, and calculated
+    Args:
+        max_movie_id (int): The maximum movie id
+        max_user_id (int): The maximum user id
+    Returns:
+        (Model): A neural network model, built up from the specified layers
+    """
+    # the number of dimensions our low dimensional embeddings should have
+    dims_of_embedding = 35
     bias = 1
     
     # inputs
+    # define an input layer by the shape of the tensors it takes
     w_inputs = Input(shape=(1,), dtype='int32')
     w = Embedding(max_work+1, dim_embedddings, name="work")(w_inputs)
     w_bis = Embedding(max_work + 1, bias, name="workbias")(w_inputs)
