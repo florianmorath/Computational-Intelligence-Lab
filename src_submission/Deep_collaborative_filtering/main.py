cc198e016b51d425ab9d7f99466d1c252ca90e96
Florian Morath <morathflorian@gmail.com>
2018-07-06 18:01:22 +0200

    refactor DCF: main.py

--- src_submission/Deep_collaborative_filtering/main.py
+++ src_submission/Deep_collaborative_filtering/main.py
@@ -1,3 +1,7 @@
+"""This module trains the deep CF model from model.py, does visualization of the loss and makes predictions.
+
+"""
+
 from model import *
 from sklearn.metrics import mean_absolute_error
 import pickle
@@ -8,11 +12,29 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
-def rmse(y_true, y_pred):
-    return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+def visualize_loss(training_loss, test_loss):
+    """ Visualize loss history.
+    
+    Args:
+        training_loss (History): Keras History object with stored training losses.
+        test_loss (History): Keras History object with stored test losses.
+        
+    Note: stores plot in current folder as 'keras_plot.png'.
+ 
+    """   
+    epoch_count = range(1, len(training_loss) + 1)
+    plt.plot(epoch_count, training_loss, 'r--')
+    plt.plot(epoch_count, test_loss, 'b-')
+    plt.legend(['Training Loss', 'Test Loss'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig('keras_plot.png')
 
+    
 def parse_line(line):
-    # parse a line of the form 'rX_cY,R'
+    """ Parse a line of the form 'rX_cY,R'.
+    
+    """
     key, value = line.split(",")
     rating = int(value)
     row_string, col_string = key.split("_")
@@ -21,54 +43,50 @@ def parse_line(line):
     return row, col, rating
 
 
+def make_submission(predictions, file_name):
+    """ Create csv submission file based on predicitons.
+    
+    """
+    ratings_a = []
+    for pred in predictions:
+        ratings_a.append(pred[0])
 
+    ratings = []
+    with open('sampleSubmission.csv') as file:
+        file.readline()
+        for line in file:
+            row, col, rat = parse_line(line)
+            ratings.append((row,col,rat))
+
+    # Build output string
+    output = "Id,Prediction\n"
+    for (row, col, rat) in ratings:
+        output += "r%d_c%d,%f\n" % (row , col, ratings_a.pop(0))
+
+    # Write file 
+    with open(file_name, 'w') as output_file:
+        output_file.write(output)
+
+        
+        
 # get the data
-train, test, max_user, max_work = get_data()
+train = get_train_data()
+test = get_test_data()
+max_user = max(train["userId"].tolist())
+max_work = max(train["movieId"].tolist())
 
 # get the model
-model = get_model(max_work, max_user)
+model = build_model(max_work, max_user)
 
 # train the model
-history = model.fit([get_array(train["movieId"]), get_array(train["userId"])], get_array(train["rating"]), nb_epoch=20,
-                    validation_split=0.2, verbose=2)
+# note: only use validation_split for visulaization and parameter search.
+history = model.fit([get_array(train["movieId"]), get_array(train["userId"])], get_array(train["rating"]), nb_epoch=20, validation_split=0.2, verbose=2)
 
-# Get training and test loss histories
+# Get training and test loss histories for visualization
 training_loss = history.history['loss']
 test_loss = history.history['val_loss']
+visualize_loss(training_loss, test_loss)
 
-# Create count of the number of epochs
-epoch_count = range(1, len(training_loss) + 1)
-
-# Visualize loss history
-plt.plot(epoch_count, training_loss, 'r--')
-plt.plot(epoch_count, test_loss, 'b-')
-plt.legend(['Training Loss', 'Test Loss'])
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.savefig('keras_plot.png')
-plt.show();
-
-
-# make predictions
+# make predictions and submission file
 predictions = model.predict([get_array(test["movieId"]), get_array(test["userId"])])
-
-# create csv submission file of predictions
-ratings_a = []
-for pred in predictions:
-    ratings_a.append(pred[0])
-
-ratings = []
-with open('sampleSubmission.csv') as file:
-    file.readline()
-    for line in file:
-        row, col, rat = parse_line(line)
-        ratings.append((row,col,rat))
-
-# Build output string
-output = "Id,Prediction\n"
-for (row, col, rat) in ratings:
-    output += "r%d_c%d,%f\n" % (row , col, ratings_a.pop(0))
-
-# Write file 
-with open('sub_5.csv', 'w') as output_file:
-    output_file.write(output)
+make_submission(predictions, 'DCF_0.csv')
